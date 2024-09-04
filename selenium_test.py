ea503ed138fe4952b70b0b265d5c755b190fdf40
v.nakaz <masaki.nakazato@ves.co.jp>
2024-09-05 07:21:05 +0900

    refactoring2

--- selenium_test.py
+++ selenium_test.py
@@ -10,26 +10,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 
-TARGET_URL = "https://den2k6.github.io/actions_aws_deploy/"
-EXPECTED_TITLE = "AWS deploy test using Actions"
+TARGET_URL = "https://nakazt.github.io/actions_test/"
+EXPECTED_H1 = "GitHub Actions + Pages Sample"
 
 
-def selenium_test(url, expected_title):
+def selenium_test(url, expected_h1):
     """テストの実行"""
 
     chrome_options = Options()
     chrome_options.add_argument("--headless")  # ヘッドレスモード
 
     if os.name == "nt":
-        # Windows の場合
         current_dir = os.path.dirname(os.path.abspath(__file__))
         chrome_driver_path = os.path.join(current_dir, "chromedriver.exe")
         service = Service(executable_path=chrome_driver_path)
-        driver = webdriver.Chrome(service=service, options=chrome_options)
     elif os.name == "posix":
-        # Ubuntu の場合
         service = Service("/usr/bin/chromedriver")
-        driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
     # テスト開始
     driver.get(url)
@@ -37,15 +35,15 @@ def selenium_test(url, expected_title):
     h1_text = driver.find_element(By.TAG_NAME, "h1").text
 
     assert (
-        title == expected_title
-    ), f"タイトルが一致しません。取得されたタイトル: {title}"
+        h1_text == expected_h1
+    ), f"H1 が一致しません。取得値: {h1_text}、期待値: {EXPECTED_H1}"
 
     print(f"page title: {title}")
     print(f"h1 text:    {h1_text}")
 
-    time.sleep(1)
+    time.sleep(0)
     driver.quit()
 
 
 if __name__ == "__main__":
-    selenium_test(TARGET_URL, EXPECTED_TITLE)
+    selenium_test(TARGET_URL, EXPECTED_H1)
